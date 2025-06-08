30e3512179bce3d9a889f549f859bbec095f45d8
DulaKavindi <dularikavindi6@gmail.com>
2025-06-08 21:47:14 +0530

    personal-portfolio

---
+++ personal-portfolio-app/src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from "./Hero.module.css";
+import Me from "../../assets/hero/Me.png";
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I,m Dulari Kavindi</h1>
+        <p className={styles.description}>I.m a Full-stack developer</p>
+    
+      </div>
+
+        <img className={styles.heroImg} src={Me} alt="Hero image of me"/>
+     
+     <div className={styles.topBlur}/>
+     <div className={styles.bottomBlur}/>
+    </section>
+  )
+}
+
+export default Hero
