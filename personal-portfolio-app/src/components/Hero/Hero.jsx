b24023774fa60a9843a1bd78f3a44f5ce77c8666
DulaKavindi <dularikavindi6@gmail.com>
2025-06-15 10:50:36 +0530

    personal-portfolio-app

--- personal-portfolio-app/src/components/Hero/Hero.jsx
+++ personal-portfolio-app/src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I,m Dulari Kavindi</h1>
-        <p className={styles.description}>I.m a Full-stack developer</p>
+        <h1 className={styles.title}>Hi... I'm Dulari Kavindi</h1>
+        <p className={styles.description}>I'm an enthusiastic and dedicated IT student at the Institute of University of Moratuwa, passionate about web development. I enjoy building clean, responsive applications and continuously expanding my skills to become a full-stack developer.</p>
     
       </div>
 
---
+++ personal-portfolio-app/src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./ProjectCard.module.css";
+import projectImg from "../../assets/projects/project.png";
+
+const ProjectCard = ({
+  project: { title, description, skills, source },
+}) => {
+  return (
+    <div className={styles.container}>
+      <img
+        src={projectImg}
+        alt={`Image of ${title}`}
+        className={styles.image}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => (
+          <li key={id} className={styles.skill}>
+            {skill}
+          </li>
+        ))}
+      </ul>
+      <div className={styles.links}>
+        <a
+          href={source}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          Source
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
