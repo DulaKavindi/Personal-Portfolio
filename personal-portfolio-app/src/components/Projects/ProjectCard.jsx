0481f3182a4aeec7fdfdfeccca3dba99516eeda1
DulaKavindi <dularikavindi6@gmail.com>
2025-06-16 18:50:08 +0530

    Personal-Portfolio

--- personal-portfolio-app/src/components/Projects/ProjectCard.jsx
+++ personal-portfolio-app/src/components/Projects/ProjectCard.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import styles from "./ProjectCard.module.css";
-import projectImg from "../../assets/projects/project.png";
+
 
 const ProjectCard = ({
-  project: { title, description, skills, source },
+  project: { title, description, skills, source, image },
 }) => {
   return (
     <div className={styles.container}>
       <img
-        src={projectImg}
+        src={new URL(`../../assets/projects/${image}`, import.meta.url).href}
         alt={`Image of ${title}`}
         className={styles.image}
       />
+
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
